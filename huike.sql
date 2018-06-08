8c5a0374847593b1acc3b1016b60bf8ebb6ed63a
lailaqishi <15162125794@163.com>
2018-06-08 09:33:56 +0800

    huike首页数据库

---
+++ huike.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `huike_first`;
+CREATE TABLE `huike_first` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hangshu` varchar(255) NOT NULL,
+  `pics` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
